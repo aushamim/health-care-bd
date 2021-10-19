20ded44e28792f7a1f13790850009b52ee2b0f5b
aushamim <au.shamim0@gmail.com>
2021-10-19 23:07:33 +0600

    Initial Upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# HealthCare BD
+
+### Always Ready to Help
+
+- Features
+- - We Are Always Ready to Help.
+- - 24/7 Emergency.
+- - A Long Selection of Services.
+- - Latest Covid-19 Data.
+- - Latest Medical News.
+
+## [Live Site]()
---
+++ src/Context/FIrebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLxasylMyw-y-iHMpOWY5W3QU47xc3o1w",
+  authDomain: "healthcarebd-6a2d9.firebaseapp.com",
+  projectId: "healthcarebd-6a2d9",
+  storageBucket: "healthcarebd-6a2d9.appspot.com",
+  messagingSenderId: "273252707362",
+  appId: "1:273252707362:web:0fe3e7d1ef6fbab2ebee7b",
+};
+export default firebaseConfig;
